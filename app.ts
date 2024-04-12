d195d7cf412be985672204b66f12dfdba446dcaa
en-22 <132026305+en-22@users.noreply.github.com>
2024-04-12 14:11:37 -0300

    Add files via upload

--- app.ts
+++ app.ts
@@ -4,18 +4,22 @@ import routes from './routes';
 import path from 'path';
 import swaggerDocs from './config/openAPISpec';
 import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 
 // Express configuration
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(cors());
 
 // Swagger UI route
 app.use(
     '/api-docs',
     swaggerUi.serve,
     swaggerUi.setup(swaggerDocs, {
-        explorer: true,
+        swaggerOptions: {
+            supportedSubmitMethods: ['get'],
+        },
     })
 );
 
@@ -25,4 +29,4 @@ app.use('/uploads', express.static(path.basename('uploads')));
 
 // Server starting point
 const server = app.listen(3000);
-console.log('Server running on port 3000');
+console.log('Server running on port 3000 (CORS enabled)');
